833d160a3f21ee74d367f3e3ec654af228c29415
eune6420 <eune6420@gmail.com>
2015-07-31 16:36:59 +0900

    Create ExOh.rb

---
+++ ExOh.rb
@@ -0,0 +1,15 @@
+def ExOh(str)
+
+  # code goes here
+ a = str.count "x"
+ b = str.count "o"
+  result = "true"
+  if a == b
+    result = "true"
+  else
+    result = "false"
+  end
+
+    return  result
+         
+end
